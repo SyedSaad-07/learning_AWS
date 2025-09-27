a77e5f5795080f1c2899e92e10b3088a96bfb3b8
SyedSaad-07 <syed.07saad@gmail.com>
2025-09-28 03:56:39 +0500

    testing to add another ec2 instance

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/new-route", (req, res)=> {
 
 app.get("/test", (req, res)=> {
     console.log("Testing request!");
-    return res.json({statusCode: 200, message: "Request Working fineeeeeeeeeeeeee "});
+    return res.json({statusCode: 200, message: "Request Working very fine "});
 })
 
 app.listen(3000, () => {
